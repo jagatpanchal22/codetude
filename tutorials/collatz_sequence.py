2b53657b15753cbf105a786b60cb81cdb8c99d1f
Jagat Panchal <jagatpanchal22@gmail.com>
2020-04-30 21:34:46 +0530

    adding logic and updating integrate.yml

--- tutorials/collatz_sequence.py
+++ tutorials/collatz_sequence.py
@@ -1,5 +1,29 @@
+def collatz(number):
+    """
+    A Collatz sequence which checks a number is even or odd returns values according
+    :param number: takes an integer value
+    :return: returns an integer value
+    """
+    if number % 2 == 0:
+        return number // 2
+    else:
+        return 3 * number + 1
+
+
 def main():
-    print("Collatz Program")
+    """
+    A main function which is starting point for program
+    :return:
+    """
+    print("====== Collatz Program =====")
+    try:
+        userInput = int(input("Enter Value: "))
+        res = collatz(userInput)
+        print(res)
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+    print("============================")
+
 
 if __name__ == "__main__":
     main()
